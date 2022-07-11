17506403d38c95dfbf661f6ddbcc8eebbea5bc97
AmauryD <deflorenne.amaury@gmail.com>
2022-07-11 17:22:54 +0200

    p

--- sql.js
+++ sql.js
@@ -3,7 +3,8 @@ import postgres from 'postgres'
 const sql = postgres({
     password: "postgres",
     host: "localhost",
-    
+    database: "postgres",
+    user: "postgres"
  });
 
  await sql`SELECT 1;`;
